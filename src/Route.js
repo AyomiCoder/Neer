cdfeeb38184ecfffe3f77a8a2898311555fd795d
Aluko Ayomide <alukoayomide623@gmail.com>
2022-06-23 10:25:46 +0100

    initial commit

---
+++ src/Route.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+
+function Route() {
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route />
+   </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default Route
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+function Home() {
+  return (
+    <div className='homepage'>
+    </div>
+  )
+}
+
+export default Home
